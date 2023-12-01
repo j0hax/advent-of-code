23df8857f98d45eeb3742943196b8acdeb265527
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2023-12-01 18:09:35 +0100

    Solve problem two

--- trebuchet/calibration.go
+++ trebuchet/calibration.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -9,42 +10,92 @@ import (
 	"unicode"
 )
 
+// Map containing spelled digits.
+var digits = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+/*
+The big problem with spelled out numbers:
+
+you can't simply replace the word with the digit in the string:
+e.g. "oneight" would be "1ight," which would evaluate to 11 and not 18.
+
+As such, a simple forward parser had to be implemented.
+*/
+func parseNum(dat []rune, index int, includeWords bool) int {
+	// Part one: find a digit
+	if unicode.IsDigit(dat[index]) {
+		return int(dat[index] - '0')
+	}
+
+	// Part two: find a letter, check if it matches what we know
+	if includeWords {
+		for k, v := range digits {
+			// quick oob check
+			indexEnd := index + len(k)
+			if indexEnd > len(dat) {
+				continue
+			}
+
+			// We know the key. Now we compare the key to the next n letters of data
+			nextTokens := string(dat[index:indexEnd])
+			if k == nextTokens {
+				log.Printf("Match: %s means %d\n", nextTokens, v)
+				return v
+			}
+		}
+	}
+
+	return -1
+}
+
 // Returns the sum of the first and last digits contained in line
-func SumLine(line string) int {
+func SumLine(line string, includeWords bool) int {
+	dat := []rune(line)
 	first := 0
 	last := 0
 
-	// Convert string to rune array
-	dat := []rune(line)
-
 	// Forward search
 	for i := 0; i < len(dat); i++ {
-		if unicode.IsDigit(dat[i]) {
-			last = int(dat[i] - '0')
+		res := parseNum(dat, i, includeWords)
+		if res > 0 {
+			first = res
+			break
 		}
 	}
 
 	// Backward search
 	for i := len(dat) - 1; i >= 0; i-- {
-		if unicode.IsDigit(dat[i]) {
-			first = int(dat[i] - '0')
+		res := parseNum(dat, i, includeWords)
+		if res > 0 {
+			last = res
+			break
 		}
 	}
 
-	combine := first*10 + last
+	result := first*10 + last
 
-	log.Printf("%v + %v = %v\n", first, last, combine)
+	log.Printf("%v => (%d, %d) = %d\n", line, first, last, result)
 
-	return combine
+	return result
 }
 
-func SumDocument(doc io.Reader) int {
+func SumDocument(doc io.Reader, toDecimal bool) int {
 	sum := 0
 
 	scanner := bufio.NewScanner(doc)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		sum += SumLine(scanner.Text())
+		sum += SumLine(scanner.Text(), toDecimal)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Panic(err)
@@ -54,7 +105,10 @@ func SumDocument(doc io.Reader) int {
 }
 
 func main() {
-	result := SumDocument(os.Stdin)
+	spelled := flag.Bool("spelled", false, "Include spelled words")
+	flag.Parse()
+
+	result := SumDocument(os.Stdin, *spelled)
 
 	fmt.Printf("Result:\t%v\n", result)
 }
--- trebuchet/calibration_test.go
+++ trebuchet/calibration_test.go
@@ -5,16 +5,35 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T) {
+func TestSumBasic(t *testing.T) {
 	input := `1abc2
 	pqr3stu8vwx
 	a1b2c3d4e5f
 	treb7uchet`
 
 	r := strings.NewReader(input)
-	s := SumDocument(r)
+	s := SumDocument(r, false)
 
 	expected := 142
+
+	if s != expected {
+		t.Fatalf(`Sum() from example should be %v, got %v`, expected, s)
+	}
+}
+
+func TestSumSpelled(t *testing.T) {
+	input := `two1nine
+	eightwothree
+	abcone2threexyz
+	xtwone3four
+	4nineeightseven2
+	zoneight234
+	7pqrstsixteen`
+
+	r := strings.NewReader(input)
+	s := SumDocument(r, true)
+
+	expected := 281
 
 	if s != expected {
 		t.Fatalf(`Sum() from example should be %v, got %v`, expected, s)
